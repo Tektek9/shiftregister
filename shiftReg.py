0ace484655543a99c2bff3c7e8c0cd4297b2fd72
CukiD <40711562+Tektek9@users.noreply.github.com>
2024-02-23 15:38:32 +0700

    Update shiftReg.py

--- shiftReg.py
+++ shiftReg.py
@@ -3,9 +3,10 @@ import time
 import sys
 
 class sr:
-  def __init__(self, p, t):
+  def __init__(self, p, t, nl):
     self.p = p
     self.t = t
+    self.nl = nl
 
   def inisialisasi(self):
       GPIO.setmode(GPIO.BCM)
@@ -40,13 +41,14 @@ class sr:
       print(f"\nPin Default\n  Data: {self.p[0]}\n  Latch: {self.p[1]}\n  Clock: {self.p[2]}\n")
       self.inisialisasi()
       self.digit()
-      nul = '0' * 8
       self.proses()
       time.sleep(2)
       GPIO.output(int(self.p[1]), 1)
-      print(f"\nMengembalikan semua nilai shift register\nDigit: {nul}\n")
+      print(f"\nMengembalikan semua nilai shift register\nDigit: {self.nl}\n")
+      GPIO.cleanup()
 
     except KeyboardInterrupt:
+      GPIO.cleanup()
       print("\nProgram Close ya\n")
 
   def custom(self):
@@ -54,26 +56,27 @@ class sr:
       print(f"\nPin Custom\n  Data: {self.p[0]}\n  Latch: {self.p[1]}\n  Clock: {self.p[2]}\n")
       self.inisialisasi()
       self.digit()
-      nul = '0' * 8
       self.proses()
       time.sleep(2)
       GPIO.output(int(self.p[1]), 1)
-      print(f"\nMengembalikan semua nilai shift register\nDigit: {nul}\n")
+      print(f"\nMengembalikan semua nilai shift register\nDigit: {self.nl}\n")
+      GPIO.cleanup()
 
     except KeyboardInterrupt:
+      GPIO.cleanup()
       print("\nProgram Close ya\n")
 
 
 if __name__ == "__main__":
   tes = []
-  
+  nul = '0' * 8
   if len(sys.argv) == 4:
     newpin = sys.argv[-3:]
-    run = sr(newpin, tes)
+    run = sr(newpin, tes, null)
     run.custom()
   elif len(sys.argv) == 1:
     pins = [16, 12, 5]
-    run = sr(pins, tes)
+    run = sr(pins, tes, null)
     run.default()
   elif len(sys.argv) == 2 and ((sys.argv[1] == "-h") or (sys.argv[1] == "--help")):
     print("\nSHIFT REGISTER for 74HC595\n\nInfo:\n  Data - isikan pin data\n  Latch - isikan pin latch\n  Clock - isikan pin clock\n\nPenggunaan:\n  Default:\n    python.exe shiftReg.py\n\n  Custom:\n    python.exe shiftReg.py [data] [latch] [clock]\n")
